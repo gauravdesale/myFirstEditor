216201ca2bb6b1f7483c15e9684c23b76c0fc316
gauravdesale <desalegaurav60@gmail.com>
2017-09-23 19:15:24 -0500

    this is adding more understandable comments

--- canonicalMain.c
+++ canonicalMain.c
@@ -6,14 +6,14 @@
 
 struct termios saveInput;//made to save the inputs from the user 
 
-int disableRawMode(){
+int disableRawMode(){//raw mode is basically how it outputs everything we input again which is what we want
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &saveInput);//disabling the raw mode
-}
+}//write terminal outputs back out
 int enableRawMode(){
-	tcgetattr(STDIN_FILENO, &orig_termios);
-  	atexit(disableRawMode);
+	tcgetattr(STDIN_FILENO, &orig_termios);//the terminal attributes was about getting the attributes and putting them into the struct
+  	atexit(disableRawMode);//we dont want this carrying over even we exit
   	struct termios raw = orig_termios;//makes the raw struct to the one we declated before
-  	raw.c_iflag &= ~(ICRNL | IXON);
+  	raw.c_iflag &= ~(ICRNL | IXON);//this is an input flag but look more into this
   	raw.c_oflag &= ~(OPOST);//setting a bunch of control commands to false
   	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);//this is turning of setattr
@@ -22,11 +22,11 @@ int enableRawMode(){
   int main(){
 	enableRawMode();
 	char *c;
-	while(read(STDIN_FILENO, &c, 1) != 0  && c != 'q');
+	while(read(STDIN_FILENO, &c, 1) != 0  && c != 'q');//check the validity of all the inputs 
 	if(iscntrl(c))
-		printf("%d\n", c);
+		printf("%d\n", c);//check if its a control command
 	else if(isdigit(c))
-		prinf("this is a digit lol");
+		prinf("this is a digit lol");//debugging
 		printf("%d ('%c')\r\n", c, c);//display keypresses is they are not a control + z or something
 	return 0;
 }
