d9dd5efd583c1195843dca0b6c6d6fa2cf32d2c1
gauravdesale <desalegaurav60@gmail.com>
2017-09-23 19:06:37 -0500

    disbling more stuff

--- canonicalMain.c
+++ canonicalMain.c
@@ -4,21 +4,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-struct termios saveInput;
+struct termios saveInput;//made to save the inputs from the user 
 
 int disableRawMode(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &saveInput);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &saveInput);//disabling the raw mode
 }
 int enableRawMode(){
-	tcgetattr(STDIN_FILENO, &saveInput);
-	atexit(disableRawMode);
-	struct termios raw = &saveInput;
-	RAW.C_iflag &= ~(IXON);
-	raw.c_lflag &= ~(ECHO | ICANON | SIGINT | SIGTSTP | IEXTEN);
-	//this is the not the input flag like the one above since it does not take in any sort of inputs of the command controls
-	//disable the echo command
-	tcsetattr(STDIN_FILEIN, TCSANOW, &raw);
-}int main(){
+	tcgetattr(STDIN_FILENO, &orig_termios);
+  	atexit(disableRawMode);
+  	struct termios raw = orig_termios;//makes the raw struct to the one we declated before
+  	raw.c_iflag &= ~(ICRNL | IXON);
+  	raw.c_oflag &= ~(OPOST);//setting a bunch of control commands to false
+  	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);//this is turning of setattr
+}//turning off all the output processing
+  
+  int main(){
 	enableRawMode();
 	char *c;
 	while(read(STDIN_FILENO, &c, 1) != 0  && c != 'q');
@@ -26,7 +27,7 @@ int enableRawMode(){
 		printf("%d\n", c);
 	else if(isdigit(c))
 		prinf("this is a digit lol");
-		printf("%d ('%c')\n", c, c);//display keypresses is they are not a control + z or something
+		printf("%d ('%c')\r\n", c, c);//display keypresses is they are not a control + z or something
 	return 0;
 }
 
